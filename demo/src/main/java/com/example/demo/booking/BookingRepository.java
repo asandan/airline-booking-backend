f7d5e659cfc45f2363c8428c78db333c33ea8f35
asandan <assanali.danybayev@gmail.com>
2023-11-07 14:57:40 +0600

    Implemented API

--- demo/src/main/java/com/example/demo/booking/BookingRepository.java
+++ demo/src/main/java/com/example/demo/booking/BookingRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.booking;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-
+    @Query("SELECT b FROM Booking b WHERE b.user.id=?1")
+    List<Booking> findBookingByUserId(Long userId);
 }
