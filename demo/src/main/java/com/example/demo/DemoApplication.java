d4593d45eaa4486ad45c2e1e235ac32c2384b160
asandan <assanali.danybayev@gmail.com>
2023-11-18 02:37:45 +0600

    Added faker dependency

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,7 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -18,6 +16,8 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
 
     }
+
+
 
 
 }
--- demo/src/main/java/com/example/demo/user/UserRepository.java
+++ demo/src/main/java/com/example/demo/user/UserRepository.java
@@ -1,6 +1,5 @@
 package com.example.demo.user;
 
-import com.example.demo.student.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
